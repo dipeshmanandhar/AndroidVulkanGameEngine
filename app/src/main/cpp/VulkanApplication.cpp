9d189ce6ea315eb7bbbbd444b475bdc1fff2a462
Dipesh Manandhar <dipesh_manz@yahoo.com>
2021-10-12 21:48:18 -0400

    replace const array with std::array

--- app/src/main/cpp/VulkanApplication.cpp
+++ app/src/main/cpp/VulkanApplication.cpp
@@ -1,7 +1,7 @@
 #include "VulkanApplication.h"
 
 #include <stdexcept>
-#include <type_traits>
+#include <array>
 
 #include "Logging.h"
 
@@ -23,7 +23,7 @@ void VulkanApplication::run(struct android_app *app)
 void VulkanApplication::initWindow(struct android_app *app)
 {
     LOGI("Initializing window");
-    
+
     this->app = app;
 }
 
@@ -62,16 +62,16 @@ void VulkanApplication::createInstance()
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    const char *instance_extensions[] = {
+    std::array<const char *, 2> instance_extensions = {
         "VK_KHR_surface",
-        "VK_KHR_android_surface"
+        "VK_KHR_android_surface",
     };
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
-    createInfo.enabledExtensionCount = std::extent<decltype(instance_extensions)>::value;
-    createInfo.ppEnabledExtensionNames = instance_extensions;
+    createInfo.enabledExtensionCount = instance_extensions.size();
+    createInfo.ppEnabledExtensionNames = instance_extensions.data();
     createInfo.enabledLayerCount = 0;
 
     if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
